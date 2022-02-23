771062f2d6f7c4cd336af5add2f50881c0a93422
Selenium Master <services@seleniummaster.com>
2022-02-23 06:26:05 -0500

    JUnit framework

--- src/test/java/com/unitedcoder/integrationtest/cubecartjunit/CubecartJUnitAutomationDemo.java
+++ src/test/java/com/unitedcoder/integrationtest/cubecartjunit/CubecartJUnitAutomationDemo.java
@@ -8,21 +8,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 public class CubecartJUnitAutomationDemo {
- static   FunctionLibrary functionLibrary=new FunctionLibrary(); //created an instance of the function library class
- static List<Product> productList=new ArrayList<>();   //to hold the test product
-   static String configFile="config-qa.properties";
-   static String timestamp=functionLibrary.getCurrentTimestampWithoutDash();
+    static FunctionLibrary functionLibrary = new FunctionLibrary();
+    static List<Product>productList = new ArrayList<>();
+    static String configFile="config-qa.properties";
+   static String timestamp =functionLibrary.getCurrentTimestampWithoutDash();
     @BeforeAll
     public static void setUp()
     {
         //open browser and login
         functionLibrary.openBrowser();
-       ApplicationConfig applicationConfig=new ApplicationConfig();
-       String username=applicationConfig.readConfigProperties(configFile,"username");
-       String password=applicationConfig.readConfigProperties(configFile,"password");
-       LoginUser loginUser=new LoginUser(username,password);
-       functionLibrary.login(loginUser);
+        ApplicationConfig applicationConfig= new  ApplicationConfig();
+        String username=applicationConfig.readConfigProperties(configFile,"username");
+        String password=applicationConfig.readConfigProperties(configFile,"password");
+        LoginUser loginUser= new LoginUser(username,password);
+        functionLibrary.login(loginUser);
+
 
     }
     @Test
@@ -30,37 +32,43 @@ public class CubecartJUnitAutomationDemo {
     @Order(1)
     public void addCustomerTest()
     {
-     Customer testCustomer=new Customer("Dolkun"+timestamp,"Tarim"+timestamp,"test"+timestamp+"@test.com");
-     boolean testStatus= functionLibrary.addCustomer(testCustomer) ;
-       Assertions.assertTrue(testStatus);
+        String timestamp=functionLibrary.getCurrentTimestamp();
+        Customer testCustomer= new Customer("Mayire"+timestamp,"Tursun"+timestamp,"test"+timestamp+"@gmail.com");
+         boolean testStatus =functionLibrary.addCustomer(testCustomer);
+         Assertions.assertTrue(testStatus);
+
     }
     @Test
     @Tag("Regression Test")
     @Order(2)
     public void addProductTest()
     {
-       Product product=new Product("iphone","iphone","cat_35","Apple");
 
-       Product testProduct=new Product(product.getProductName()+timestamp,product.getProductCode()+timestamp,
-               product.getProductCategory(),product.getProductManufacturer());
-       productList.add(testProduct);
-       boolean testStatus=functionLibrary.addProduct(product,timestamp);
-       Assertions.assertTrue(testStatus);
+        Product product=new Product("iphone","iphone","cat_35","Apple");
+        Product testProduct= new Product(product.getProductName()+timestamp,product.getProductCode()+timestamp,
+                product.getProductCategory()+timestamp,product.getProductManufacturer()+timestamp);
+        productList.add(testProduct);
+        boolean testStatus=functionLibrary.addProduct(product,timestamp);
+        Assertions.assertTrue(testStatus);
+
     }
     @Test
-    @Tag("Regession Test")
+    @Tag("Regression Test")
     @Order(3)
     public void productBulkPriceChangeTest()
     {
-    boolean priceChangeTestResult= functionLibrary.priceChange(productList, ProductBulkPriceMethod.BY_AMOUNT,ProductPriceChangeAction.ADD,20);
-    Assertions.assertTrue(priceChangeTestResult);
-    }
+     boolean priceChangeTestResult = functionLibrary.priceChange(productList, ProductBulkPriceMethod.BY_AMOUNT,
+             ProductPriceChangeAction.ADD,20);
+     Assertions.assertTrue(priceChangeTestResult);
 
+    }
     @AfterAll
     public static void tearDown()
     {
-        //logout and close the browser
+     // logout and close the browser
        functionLibrary.logout();
-       functionLibrary.closeBrowser();
+        functionLibrary.closeBrowser();
+
+
     }
 }
--- src/test/java/com/unitedcoder/integrationtest/cubecartjunit/JUnitTestDemo2.java
+++ src/test/java/com/unitedcoder/integrationtest/cubecartjunit/JUnitTestDemo2.java
@@ -1,53 +1,62 @@
 package com.unitedcoder.integrationtest.cubecartjunit;
 
-import org.junit.jupiter.api.*;
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)  //default
-public class JUnitTestDemo2 {
-  @BeforeAll
-    public static void setUp()
-  {
-      System.out.println("Before all will run only once in the Junit framework before any test");
-  }
-  @BeforeEach
-  public void beforeTestInfo()
-  {
-      System.out.println("test start ....... ");
-  }
-  @AfterEach
-  public void afterTestInfo()
-  {
-      System.out.println("test end ...... ");
-  }
-  @Test
-  @Order(1)
-  @Tag("Smoke Test")
-  public void squareRootTest()
-  {
-      System.out.println("This is for testing a square root");
-      Assertions.assertTrue(Math.sqrt(9)==3.0);
-  }
-  @Test
-  @Order(2)
-  @Tag("Smoke Test")
-   public   void compareStringTest()
-  {
-      String s1="selenium";
-      String s2="selenium";
-      Assertions.assertEquals(s1,s2);
-  }
-  @Test
-  @Order(3)
-  @Tag("Regression Test")
-  public void mathPowerTest()
-  {
-      System.out.println("This is for testing a power of a number");
-      Assertions.assertTrue(Math.pow(10,2)==100);
-  }
-  @AfterAll
-    public static void tearDown()
-  {
-      System.out.println("After all will run only once in the JUnit framework after any test");
-  }
 
-}
+import org.junit.jupiter.api.*;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)// default
+
+public class JUnitTestDemo2 {
+
+
+    @BeforeAll
+    public static void setUp()
+    {
+        System.out.println("Before all will run only once in the Junit framework before ant test");
+    }
+    @BeforeEach
+    public void beforeTestInfo()
+    {
+        System.out.println("test start .........");
+    }
+    @AfterEach
+    public void afterTestInfo()
+    {
+        System.out.println("test end........");
+    }
+    @Order(1)
+    @Test
+    @Tag("Smoke Test")
+    public void squareRootTest()
+    {
+        System.out.println("This is for testing a square  root");
+        Assertions.assertTrue(Math.sqrt(9)==3.0);
+    }
+    @Order(2)
+    @Test
+    @Tag("Smoke Test")
+    public void compareStringTest() {
+        String s1 = "selenium";
+        String s2 = "selenium";
+        Assertions.assertEquals(s1, s2);
+
+    }
+    @Test
+    @Order(3)
+    @Tag("Regression Test")
+     public void mathPowerTest()
+    {
+        System.out.println("This is for testing a power of a number ");
+        Assertions.assertTrue(Math.pow(10,2)==100);
+    }
+
+            @AfterAll
+            public static void tearDown()
+
+            {
+                System.out.println("after all will run only once in the JUint framework after any test");
+            }
+
+        }
+
+
+
 
